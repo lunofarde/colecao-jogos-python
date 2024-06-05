afdb362787b96f5ac190d6363f45b97cacccc0f8
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2024-06-05 14:40:52 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+print("""
+█▀▀ █▀█ █░░ █▀▀ █▀▀ ▄▀█ █▀█   █▀▄ █▀▀   ░░█ █▀█ █▀▀ █▀█ █▀
+█▄▄ █▄█ █▄▄ ██▄ █▄▄ █▀█ █▄█   █▄▀ ██▄   █▄█ █▄█ █▄█ █▄█ ▄█
+""")
+
+print('1. Cadastrar jogo')
+print('2. Listar jogos')
+print('3. Ativar jogo')
+print('4. sair\n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print(f'Você escolheu a opção: {opcao_escolhida}')
