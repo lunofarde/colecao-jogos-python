89413160d7aa655de57f764ec8f4ad01ea8aa4e6
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2024-06-06 10:09:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,39 @@
-print("""
-█▀▀ █▀█ █░░ █▀▀ █▀▀ ▄▀█ █▀█   █▀▄ █▀▀   ░░█ █▀█ █▀▀ █▀█ █▀
-█▄▄ █▄█ █▄▄ ██▄ █▄▄ █▀█ █▄█   █▄▀ ██▄   █▄█ █▄█ █▄█ █▄█ ▄█
-""")
+import os
 
-print('1. Cadastrar jogo')
-print('2. Listar jogos')
-print('3. Ativar jogo')
-print('4. sair\n')
+def exibir_nome_do_programa():
+    print("""
+    █▀▀ █▀█ █░░ █▀▀ █▀▀ ▄▀█ █▀█   █▀▄ █▀▀   ░░█ █▀█ █▀▀ █▀█ █▀
+    █▄▄ █▄█ █▄▄ ██▄ █▄▄ █▀█ █▄█   █▄▀ ██▄   █▄█ █▄█ █▄█ █▄█ ▄█
+    """)
+
+def exibir_opcoes():
+    print('1. Cadastrar jogo')
+    print('2. Listar jogos')
+    print('3. Ativar jogo')
+    print('4. sair\n')
 
 def finalizar_app():
+    os.system('cls')
     print('Encerrando programa')
 
-opcao_escolhida = int(input('Escolha uma opção: '))
-print(f'Você escolheu a opção: {opcao_escolhida}')
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    print(f'Você escolheu a opção: {opcao_escolhida}')
 
-if opcao_escolhida == 1:
-    print('Cadastro de jogos')
-elif opcao_escolhida == 2:
-    print('Listar jogos')
-elif opcao_escolhida == 3:
-    print('Ativar jogo')
-else:
-    finalizar_app()
+    if opcao_escolhida == 1:
+        print('Cadastro de jogos')
+    elif opcao_escolhida == 2:
+        print('Listar jogos')
+    elif opcao_escolhida == 3:
+        print('Ativar jogo')
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
 
 
-# if(opcao_escolhida == 1){
-
-# }
+if __name__ == '__main__':
+    main()
