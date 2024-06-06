73c044ae7c684fcb90f581b20d99710263d9bd63
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2024-06-06 09:04:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,22 @@ print('2. Listar jogos')
 print('3. Ativar jogo')
 print('4. sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
+def finalizar_app():
+    print('Encerrando programa')
+
+opcao_escolhida = int(input('Escolha uma opção: '))
 print(f'Você escolheu a opção: {opcao_escolhida}')
+
+if opcao_escolhida == 1:
+    print('Cadastro de jogos')
+elif opcao_escolhida == 2:
+    print('Listar jogos')
+elif opcao_escolhida == 3:
+    print('Ativar jogo')
+else:
+    finalizar_app()
+
+
+# if(opcao_escolhida == 1){
+
+# }
